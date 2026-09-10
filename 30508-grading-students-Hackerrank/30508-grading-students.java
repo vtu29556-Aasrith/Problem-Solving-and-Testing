50da7a12fe6bdcbadf1ea7b7e22c613f2e786078
vtu29556-Aasrith <vtu29556@veltech.edu.in>
2026-09-10 12:04:02 +0530

    Force committed solution - Time: O(n), Space: O(n)

--- 30508-grading-students-Hackerrank/30508-grading-students.java
+++ 30508-grading-students-Hackerrank/30508-grading-students.java
@@ -1,7 +1,17 @@
 import java.io.*;
 import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
 class Result {
+
+    /*
+     * Complete the 'gradingStudents' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY grades as parameter.
+     */
 
     public static List<Integer> gradingStudents(List<Integer> grades) {
         List<Integer> roundedGrades = new ArrayList<>();
@@ -26,28 +36,29 @@ class Result {
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
         int gradesCount = Integer.parseInt(bufferedReader.readLine().trim());
 
-        List<Integer> grades = new ArrayList<>();
-
-        for (int i = 0; i < gradesCount; i++) {
-            int gradesItem = Integer.parseInt(bufferedReader.readLine().trim());
-            grades.add(gradesItem);
-        }
+        List<Integer> grades = IntStream.range(0, gradesCount).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine().replaceAll("\\s+$", "");
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+            .map(String::trim)
+            .map(Integer::parseInt)
+            .collect(toList());
 
         List<Integer> result = Result.gradingStudents(grades);
 
-        for (int i = 0; i < result.size(); i++) {
-            bufferedWriter.write(String.valueOf(result.get(i)));
-
-            if (i != result.size() - 1) {
-                bufferedWriter.write("\n");
-            }
-        }
-
-        bufferedWriter.newLine();
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining("\n"))
+            + "\n"
+        );
 
         bufferedReader.close();
         bufferedWriter.close();
