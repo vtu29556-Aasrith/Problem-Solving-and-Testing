f8613e0a435a65f7249e9c0c343f60b282ab5413
vtu29556-Aasrith <vtu29556@veltech.edu.in>
2026-09-10 12:27:10 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 11961-java-exception-handling-trycatch-Hackerrank/11961-java-exception-handling-trycatch.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        try {
+            int x = scan.nextInt();
+            int y = scan.nextInt();
+            System.out.println(x / y);
+        } catch (InputMismatchException e) {
+            System.out.println(e.getClass().getName());
+        } catch (ArithmeticException e) {
+            System.out.println(e);
+        } finally {
+            scan.close();
+        }
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
